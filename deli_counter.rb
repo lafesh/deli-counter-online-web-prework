8de64c825d147476f8d3b0a96b0f037404f0d447
Gina Fedder <ginafedder@yahoo.com>
2018-09-17 23:13:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,24 +6,29 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     name_index = []
-    katz_deli.to_enum.with_index(1).each do |name, index|
+    katz_deli.each.with_index(1) do |name, index|
       name_index.push("#{index}. #{name}")
     end
     puts "The line is currently: #{name_index.join(" ")}"
   end 
 end 
 
-def take_a_number(katz_deli, string)
-  katz_deli.push(string)
-  puts "Welcome, #{string}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli)
+  if katz_deli.length == 0 
+    katz_deli.push(1)
+  else
+    number = katz_deli[-1] + 1
+    katz_deli.push(number)
+  end
+  puts "You are number #{katz_deli[-1]} in line."
 end
+
+#katz_deli = ["bob", 'amy', "sam", "chad"]
+
 
 def now_serving(katz_deli)
   if katz_deli.length > 0
-    katz_deli.each do |name|
-      puts "Currently serving #{name}."
-      return katz_deli.shift
-    end 
+    puts "Currently serving #{katz_deli.shift}."
   else
     puts "There is nobody waiting to be served!"
   end 
